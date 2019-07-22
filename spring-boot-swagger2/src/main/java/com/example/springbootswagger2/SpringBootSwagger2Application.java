bd33b62ac214517edeaba16eff3bef87a10673f7
unknown <vskreddy652@yahoo.com>
2019-07-22 09:08:54 +0530

    actuator

---
+++ spring-boot-swagger2/src/main/java/com/example/springbootswagger2/SpringBootSwagger2Application.java
@@ -0,0 +1,16 @@
+package com.example.springbootswagger2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/*
+ URL to access swagger documentation:
+ http://localhost:8080/swagger2-demo/swagger-ui.html
+ */
+@SpringBootApplication
+public class SpringBootSwagger2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootSwagger2Application.class, args);
+	}
+}
